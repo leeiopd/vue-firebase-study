d92e0a0713b6779c4f13144c1f8e5a6017aa4558
leeiopd <leeiopd@gmail.com>
2020-02-26 16:53:14 +0900

    vue_2
    - vue component
      - 부모 자식 관계
      - props
      - emit
    - alias 경로 별칭사용
      - vue.config.js

---
+++ src/views/lectures/mother.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <!-- name - 아들에게 전달할 인자 (엄마 -> 아들)
+           help - 아들에게 전달받은 인자( 아들 -> 엄마)
+    -->
+    <my-son name="aaa" @help="listen"></my-son>
+    <my-son name="bbb"></my-son>
+    <my-son name="ccc"></my-son>
+    <my-son name="ddd"></my-son>
+    <my-son name="eee"></my-son>
+    <my-dougther></my-dougther>
+    <v-card-text>
+      <v-alert v-model="call" type="success">call 성공</v-alert>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import MySon from "@/components/mySon";
+import MyDougther from "@/components/myDougther";
+export default {
+  components: {
+    MySon,
+    MyDougther
+  },
+  data() {
+    return {
+      call: false
+    };
+  },
+  methods: {
+    //   엄마에게 보고 받은것을 띄움
+    listen() {
+      this.call = true;
+    }
+  }
+};
+</script>
