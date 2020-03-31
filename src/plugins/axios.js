598543f403da173b232984fa2b6550d95595663e
leeiopd <leeiopd@gmail.com>
2020-03-31 15:15:54 +0900

    frebase_14
    - firebagse_hosting
      - ./firebase.json
      - rewrites 설정
      - ./src/plugins/axios.js
      - axios baseURL 설정
      - process.env.NODE_ENV -> development/product 구분

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 import store from '../store'
+import firebaseConfig from "../../firebaseConfig"
 
 const firebaseAPI = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "https://vue-firebase-toy.firebaseapp.com/" : "http://localhost:5000/vue-firebase-toy/us-central1/",
+    baseURL: process.env.NODE_ENV === "production" ? `https://us-central1-${firebaseConfig.projectId}.cloudfunctions.net/admin` : `http://localhost:5000/${firebaseConfig.projectId}/us-central1/`,
     timeout: 5000,
     headers: { 'X-Custome-Header': 'foobar' }
 })
